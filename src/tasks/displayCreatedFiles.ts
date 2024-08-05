22d52661bcf73b2479fd864d0406f1c3b4998947
Torathion <torathion@gmail.com>
2024-08-06 00:13:36 +0200

    change: style of table

--- src/tasks/displayCreatedFiles.ts
+++ src/tasks/displayCreatedFiles.ts
@@ -1,4 +1,4 @@
-import { printTable } from 'console-table-printer'
+import { Table } from 'console-table-printer'
 import type { CreatedFileMetadata } from 'src/types/interfaces'
 import { sortByFileSize } from 'src/utils/sortByFileSize'
 
@@ -7,5 +7,9 @@ function getNumberPart(text: string): number {
 }
 
 export default function displayCreatedFiles(files: CreatedFileMetadata[]): void {
-    printTable(files.sort(sortByFileSize))
+    const p = new Table({
+        columns: [{ name: 'action' }, { name: 'folder', alignment: 'left' }, { name: 'size' }],
+        rows: files.sort(sortByFileSize)
+    })
+    p.printTable()
 }
