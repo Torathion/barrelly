47407ef9c0e3b2c9e708cd26e1225701fd3af573
Torathion <torathion@gmail.com>
2024-08-06 01:03:27 +0200

    fix: file size sorting issues

--- src/utils/sortByFileSize.ts
+++ src/utils/sortByFileSize.ts
@@ -1,11 +1,11 @@
 import { FileSizeUnits } from 'src/constants'
 import type { FileSizeable } from 'src/types'
 
-const regex = /^(\d+)(\D+)$/
+const regex = /^(\d*\.?\d+)(\D+)$/
 
 export function sortByFileSize(a: FileSizeable, b: FileSizeable): number {
     const aMatches = regex.exec(a.size)
     const bMatches = regex.exec(b.size)
     if (!aMatches || !bMatches) return -1
-    return (+bMatches[1] - +aMatches[1] + FileSizeUnits.indexOf(bMatches[2]) - FileSizeUnits.indexOf(aMatches[2])) * 0.5
+    return +bMatches[1] * 1000 ** FileSizeUnits.indexOf(bMatches[2]) - +aMatches[1] * 1000 ** FileSizeUnits.indexOf(aMatches[2])
 }
